9749c10d6d2ea9f137ea0ae397bcf3a832af8de0
vladimirchernyy <vladimir_chernyy@list.ru>
2023-10-19 16:12:28 +0400

    review vol.4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,6 +13,8 @@ DEBUG = os.getenv('DEBUG', 'False') in ('True', '1', 't')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split()
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
